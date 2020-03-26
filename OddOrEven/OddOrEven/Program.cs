5031f94da41f75842405b2f051f83b0efb4ae2ae
Sai Deekshith <msaideekshith@gmail.com>
2020-03-26 15:16:32 +0530

    To verify given number odd or even

---
+++ OddOrEven/OddOrEven/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace OddOrEven
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int x = 0;
+
+            //To Read enter number
+            x = int.Parse(Console.ReadLine());
+
+            //To Check number is even or odd
+            if (x % 2 == 0)
+                Console.WriteLine(x + " is an even number");
+            else
+                Console.WriteLine(x + " is an odd number");
+        }
+    }
+}
